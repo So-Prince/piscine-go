9f770223998c94b6621f5c3308fec32763a5bdd5
Ofuru Prince Solomon (pofuru) <ofurusolomon2000@gmail.com>
2026-02-25 13:17:43 +0100

    Create main.go

---
+++ boolean/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+type boolean int
+
+const (
+	yes boolean = 1
+	no  boolean = 0
+
+	EvenMsg = "I have an even number of arguments"
+	OddMsg  = "I have an odd number of arguments"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func even(n int) int {
+	if n%2 == 0 {
+		return 1
+	}
+	return 0
+}
+
+func isEven(nbr int) boolean {
+	if even(nbr) == 1 {
+		return yes
+	}
+	return no
+}
+
+func main() {
+	lengthOfArg := len(os.Args[1:])
+
+	if isEven(lengthOfArg) == yes {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
