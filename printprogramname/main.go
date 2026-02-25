3d6d7ea09951fe60b0d06fa385872d843f19465b
Ofuru Prince Solomon (pofuru) <ofurusolomon2000@gmail.com>
2026-02-25 13:26:35 +0100

    Create main.go

---
+++ printprogramname/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	r := os.Args[0]
+	var k int
+	for i := 0; i < len(r); i++ {
+		if r[len(r)-1-i] == '/' {
+			k = len(r) - 1 - i
+			break
+		}
+	}
+	s := r[k+1:]
+	for _, i := range s {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
+}
