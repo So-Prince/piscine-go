b72cfaceb64c62fc4f86f8e4000adb53a1affd9a
Ofuru Prince Solomon (pofuru) <ofurusolomon2000@gmail.com>
2026-02-25 12:46:20 +0100

    Create listpushfront.go

---
+++ listpushfront.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ListPushFront(l *List, data interface{}) {
+	newN := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = newN
+		l.Tail = newN
+	} else {
+		newN.Next = l.Head
+		l.Head = newN
+	}
+}
